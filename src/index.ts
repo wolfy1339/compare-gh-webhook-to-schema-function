cc62949ca91bb2579922b2ab3b820e86465edee1
wolfy1339 <webmaster@wolfy1339.com>
2021-04-11 10:07:00 -0400

    convert to express api and disable notifier

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
-import { Context, HttpRequest } from '@azure/functions';
+import dotenv from 'dotenv';
+import { IncomingHttpHeaders } from 'http';
 import 'source-map-support/register';
+import { v5 as uuid } from 'uuid';
 import { EventValidator, describeEvent, getEvent } from './github';
-import { Notifier } from './notifier';
+// import { Notifier } from './notifier';
 
-interface FnResult {
+dotenv.config();
+export interface FnResult {
   summary: string;
   errors?: unknown[];
   error?: Error;
 }
 
-interface FnResponse {
+export interface FnResponse {
   body: FnResult;
   statusCode: number;
   headers: {
@@ -17,22 +20,23 @@ interface FnResponse {
     'X-Invocation-Id': string;
   };
 }
-
 export const handler = async (
-  context: Context,
-  request: HttpRequest
+  rawBody: string,
+  headers: IncomingHttpHeaders
 ): Promise<FnResponse> => {
-  context.log(request);
+  const context = {
+    invocationId: uuid('https://hellomouse.net/', uuid.URL)
+  };
 
-  const notifier = new Notifier();
+  // const notifier = new Notifier();
 
   try {
-    const githubEvent = await getEvent(request);
-    const errors = EventValidator.validate(githubEvent, context.log);
+    const githubEvent = await getEvent(headers, rawBody);
+    const errors = EventValidator.validate(githubEvent, console);
     const eventDescription = describeEvent(githubEvent);
 
     if (errors.length) {
-      context.log.info(errors);
+      console.log(errors);
 
       return {
         body: { summary: 'oh dear, there were errors :/', errors },
@@ -48,11 +52,11 @@ export const handler = async (
     };
   } catch (err: unknown) {
     const error = err as Error;
-    const message = error.stack ?? error.message;
+    // const message = error.stack ?? error.message;
 
-    context.log.error(error);
+    console.error(error);
 
-    await notifier.send({ text: `\`\`\`${message}\`\`\`` });
+    // await notifier.send({ text: `\`\`\`${message}\`\`\`` });
 
     return {
       body: { summary: 'oh noes!', error },
