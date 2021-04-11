5b9e931e559cfc7aa2990132551a8bdcb1944d44
wolfy1339 <webmaster@wolfy1339.com>
2021-04-11 10:07:00 -0400

    convert to express api and disable notifier

--- src/github/types.ts
+++ src/github/types.ts
@@ -11,12 +11,3 @@ type GithubEventsMap = {
 
 type GithubEventName = WebhookEventName;
 export type GithubEvent = GithubEventsMap[GithubEventName];
-
-declare module '@azure/functions' {
-  export interface HttpRequestHeaders {
-    'x-github-event': GithubEventName;
-    'x-github-delivery': string;
-    'x-hub-signature': string;
-    'x-hub-signature-256': string;
-  }
-}
